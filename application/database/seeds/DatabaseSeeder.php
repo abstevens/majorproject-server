7b63915dfaeb9c20cd2e1c684896b50fe57eab30
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-06-07 20:26:42 +0200

    Uncommented all seeder classes

--- application/database/seeds/DatabaseSeeder.php
+++ application/database/seeds/DatabaseSeeder.php
@@ -11,26 +11,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
 
-//        $this->call(SchoolsTableSeeder::class);
-//        $this->call(SchoolUsersTableSeeder::class);
+        $this->call(SchoolsTableSeeder::class);
+        $this->call(SchoolUsersTableSeeder::class);
 
-//        $this->call(EventsTableSeeder::class);
-//        $this->call(EventAttendancesTableSeeder::class);
+        $this->call(EventsTableSeeder::class);
+        $this->call(EventAttendancesTableSeeder::class);
 
-//        $this->call(RolesTableSeeder::class);
-//        $this->call(RoleUsersTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(RoleUsersTableSeeder::class);
 
-//        $this->call(PermissionsTableSeeder::class);
-//        $this->call(RolePermissionsTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(RolePermissionsTableSeeder::class);
 
-//        $this->call(CoursesTableSeeder::class);
-//        $this->call(CourseUsersTableSeeder::class);
-//        $this->call(CoursePricesSeeder::class);
-//        $this->call(AwardsTableSeeder::class);
+        $this->call(CoursesTableSeeder::class);
+        $this->call(CourseUsersTableSeeder::class);
+        $this->call(CoursePricesSeeder::class);
+        $this->call(AwardsTableSeeder::class);
 
-//        $this->call(SchoolClassesTableSeeder::class);
+        $this->call(SchoolClassesTableSeeder::class);
 //        $this->call(ClassUsersTableSeeder::class); // TODO: create this seeder
 
         $this->call(AddressesTableSeeder::class);
