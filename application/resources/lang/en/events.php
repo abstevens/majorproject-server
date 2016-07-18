93ce62e29e692f89486f7e22cde1c41d38866653
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-07-18 02:10:56 +0200

    Added events translation

--- application/resources/lang/en/events.php
+++ application/resources/lang/en/events.php
@@ -5,3 +5,9 @@
  * Date: 17/07/16
  * Time: 14:10
  */
+
+return [
+    'store_failed' => 'Failed to save a new event',
+    'update_failed' => 'Failed to update the event',
+    'destroy_failed' => 'Failed to delete the event',
+];
