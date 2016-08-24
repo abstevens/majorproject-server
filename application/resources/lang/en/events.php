bc8f05f423534e25dfcf164dc48105d390a35555
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-08-24 12:16:29 +0200

    Updated events error messages

--- application/resources/lang/en/events.php
+++ application/resources/lang/en/events.php
@@ -1,7 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: AlexStevens
- * Date: 17/07/16
- * Time: 14:10
- */
+
+return [
+
+    'store_failed' => 'Failed to store the event.',
+    'update_failed' => 'Failed to update the event.',
+    'destroy_failed' => 'Failed to delete the event.',
+
+];
