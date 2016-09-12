9980c239641eefc7279e08311c15edff00523024
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-12 12:48:39 +0200

    Modified EventAttendances factory

--- application/database/factories/EventAttendancesFactory.php
+++ application/database/factories/EventAttendancesFactory.php
@@ -3,7 +3,10 @@
 use \App\EventAttendance;
 
 $factory->define(EventAttendance::class, function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisMonth;
     return [
         'status' => $faker->boolean(),
+        'created_at' => $createdAt,
+        'updated_at' => $createdAt,
     ];
 });
