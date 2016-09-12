1b221fd4d150ba85ace3b73d05ce88b8a3c23a38
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-12 12:56:14 +0200

    Modified SchoolContacts factory

--- application/database/factories/UserContactsFactory.php
+++ application/database/factories/UserContactsFactory.php
@@ -2,9 +2,9 @@
 
 use \App\UserContact;
 
-$createdAt = $faker->dateTimeThisYear;
+$factory->defineAs(UserContact::class, 'contact phone', function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisYear;
 
-$factory->defineAs(UserContact::class, 'contact phone', function (Faker\Generator $faker) use ($createdAt) {
     return [
         'value' => $faker->phoneNumber,
         'created_at' => $createdAt,
@@ -12,7 +12,9 @@ $factory->defineAs(UserContact::class, 'contact phone', function (Faker\Generato
     ];
 });
 
-$factory->defineAs(UserContact::class, 'contact email', function (Faker\Generator $faker) use ($createdAt) {
+$factory->defineAs(UserContact::class, 'contact email', function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisYear;
+
     return [
         'value' => $faker->email,
         'created_at' => $createdAt,
