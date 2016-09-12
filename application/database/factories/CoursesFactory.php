5cb77c0da3c529b8c7da285e1fbbe9c8d52983ed
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-12 12:47:18 +0200

    Courses modified seeds and factories

--- application/database/factories/CoursesFactory.php
+++ application/database/factories/CoursesFactory.php
@@ -3,8 +3,11 @@
 use \App\Course;
 
 $factory->define(Course::class, function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisYear;
     return [
         'name' => $faker->name,
         'code' => $faker->unique()->randomNumber(4, true),
+        'created_at' => $createdAt,
+        'updated_at' => $createdAt,
     ];
 });
