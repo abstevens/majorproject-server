c6499d58998f3ad659dbaf799f3f342f15471c9a
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-12 12:44:54 +0200

    Schools modified seeds, factories, migrations and models

--- application/database/factories/SchoolsFactory.php
+++ application/database/factories/SchoolsFactory.php
@@ -3,7 +3,10 @@
 use \App\School;
 
 $factory->define(School::class, function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisCentury;
     return [
         'name' => 'SAE ' . $faker->unique()->city,
+        'created_at' => $createdAt,
+        'updated_at' => $createdAt,
     ];
 });
