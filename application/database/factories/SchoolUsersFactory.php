deafa12de566f25f1ea8acb98af493d7318c872c
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-12 12:53:28 +0200

    Modified SchoolUsers factory

--- application/database/factories/SchoolUsersFactory.php
+++ application/database/factories/SchoolUsersFactory.php
@@ -3,8 +3,11 @@
 use \App\SchoolUser;
 
 $factory->define(SchoolUser::class, function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisDecade;
     return [
         // 5-AMS-504500
         'student_code' => $faker->unique()->regexify('\d-\w{3}-\d{6}'),
+        'created_at' => $createdAt,
+        'updated_at' => $createdAt,
     ];
 });
