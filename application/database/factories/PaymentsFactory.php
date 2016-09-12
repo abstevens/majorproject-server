ceb0331b0852eca5964276bd6a99106a12592799
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-12 12:50:49 +0200

    Modified Payments factory

--- application/database/factories/PaymentsFactory.php
+++ application/database/factories/PaymentsFactory.php
@@ -3,8 +3,11 @@
 use \App\Payment;
 
 $factory->define(Payment::class, function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisYear;
     return [
         'amount' => $faker->randomFloat(2, 50, 20000),
         'description' => $faker->sentence,
+        'created_at' => $createdAt,
+        'updated_at' => $createdAt,
     ];
 });
