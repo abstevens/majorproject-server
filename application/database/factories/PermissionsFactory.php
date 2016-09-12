26b0b7bfa78ea932730eba66ae5ffecaa94d6902
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-12 12:51:19 +0200

    Modified Permissions factory

--- application/database/factories/PermissionsFactory.php
+++ application/database/factories/PermissionsFactory.php
@@ -3,8 +3,11 @@
 use \App\Permission;
 
 $factory->define(Permission::class, function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisYear;
     return [
         'name' => $faker->name,
         'code' => $faker->randomNumber(6, true),
+        'created_at' => $createdAt,
+        'updated_at' => $createdAt,
     ];
 });
