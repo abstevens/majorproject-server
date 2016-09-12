b92551a79f678c25eeb501a19fa4179d64d77d97
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-12 12:56:57 +0200

    Modified Users factory and migration

--- application/database/factories/UsersFactory.php
+++ application/database/factories/UsersFactory.php
@@ -3,10 +3,15 @@
 use \App\User;
 
 $factory->define(User::class, function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisDecade;
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'username' => $faker->userName,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+        'created_at' => $createdAt,
+        'updated_at' => $createdAt,
     ];
 });
