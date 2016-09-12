a78db566aaa6bbabf4b5bd518c4d8af856ccccc7
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-12 12:31:14 +0200

    CoursePrices modified seeds, factories, migrations and models

--- application/database/factories/CoursePricesFactory.php
+++ application/database/factories/CoursePricesFactory.php
@@ -3,7 +3,10 @@
 use \App\CoursePrice;
 
 $factory->define(CoursePrice::class, function (Faker\Generator $faker) {
+    $createdAt = $faker->dateTimeThisMonth;
     return [
         'price' => $faker->randomFloat(2, 100, 10000),
+        'created_at' => $createdAt,
+        'updated_at' => $createdAt,
     ];
 });
