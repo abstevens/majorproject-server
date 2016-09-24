00d66604759f681133d4e5321394ae2546b6e5f5
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-09-24 12:50:11 +0200

    Added Brew and composer update in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew upgrade
+
+brew update
+
 brew install mariadb
 
 brew install redis
@@ -7,6 +11,8 @@ brew install redis
 brew install homebrew/php/php71
 
 brew install composer
+
+composer global update
 
 composer global require laravel/valet
 
