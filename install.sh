e2a358f0854f04da8fe8de6963a52fbdb95f4f3e
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-10-02 11:45:19 +0200

    Updated Install steps

--- install.sh
+++ install.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew upgrade
-
 brew update
 
 brew install mariadb
-
 brew install redis
-
 brew install homebrew/php/php71
-
 brew install composer
 
+brew services start mariadb
+brew services start redis
+
 composer global update
-
 composer global require laravel/valet
-
-composer install
 
 valet install
 
+cd application
 valet link api.mjp
-
+composer install
 php artisan install --seed
