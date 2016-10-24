14e44a719a4db5c86d75aff9662b4fdfda09d6e7
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-10-24 13:43:08 +0200

    added test user in seeding

--- application/database/seeds/UsersTableSeeder.php
+++ application/database/seeds/UsersTableSeeder.php
@@ -14,6 +14,15 @@ class UsersTableSeeder extends Seeder
     {
         echo "Seeding: UsersTableSeeder... ";
 
+        DB::table('users')->insert([
+            'first_name' => 'Test',
+            'last_name' => 'Account',
+            'username' => 'user1@test.com',
+            'email' => 'user1@test.com',
+            'password' => bcrypt('1111'),
+            'remember_token' => str_random(10),
+        ]);
+
         factory(User::class, 250)->create();
     }
 }
