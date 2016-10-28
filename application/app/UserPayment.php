5bee2c69e1f026aef95e6fffd35030276efa4180
Alexander Stevens <alexanderbilliestevens@gmail.com>
2016-10-28 16:18:12 +0200

    Changed Payment to UserPayment

--- application/app/UserPayment.php
+++ application/app/UserPayment.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
+    protected $hidden = [
+        'created_at', 'updated_at', 'deleted_at',
+    ];
+
     public function users()
     {
         return $this->belongsTo('App\User');
